74c0f925c0b5fb029360324b6fdde7accbb33487
turgood <turgutdursun@windowslive.com>
2014-01-17 16:32:12 +0200

    filedownload
    tanm

---
+++ fileDownload/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<title>PHP </title>
+
+<div id="frame1">
+  <ul>
+    <li><a href="download.php?filename=<?php echo base64_encode("blog.png"); ?>">Download Link 1</a></li>
+    <li><a href="download.php?filename=<?php echo base64_encode("w3-logo.png"); ?>">Download Link 2</a></li>
+    <li><a href="download.php?filename=<?php echo base64_encode("tdd.pdf"); ?>">Download Link 1</a></li>
+    <li><a href="download.php?filename=<?php echo base64_encode("w3-logo.avi"); ?>">Download Link 2</a></li>
+  </ul>
+</div>
+<br>
+</body></html>
